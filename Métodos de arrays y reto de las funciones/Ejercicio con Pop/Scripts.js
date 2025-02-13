9630b91a967f8e63c8e25b522d572e18b3ac8964
samuelito1218 <165844479+samuelito1218@users.noreply.github.com>
2025-02-13 00:28:05 -0500

    Delete Métodos de arrays y reto de las funciones/Ejercicio con Pop directory

--- Métodos de arrays y reto de las funciones/Ejercicio con Pop/Scripts.js
+++
@@ -1,3 +0,0 @@
-let juegos = ["Resident Evil" , "God of war", "God of war 2", "GTA San Andreas"]; /*Arreglo originalmente de 4 elementos*/
-juegos.pop(); /*Mediante el método se  elimina el último elemento del arreglo, y se muestra el resultado final por consola*/
-console.log(juegos);
