945e5d393044e0e06f60aed71f89066b5c3d5526
samuelito1218 <165844479+samuelito1218@users.noreply.github.com>
2025-02-13 00:28:51 -0500

    Delete Métodos de arrays y reto de las funciones/Ejercicio con Unshift directory

--- Métodos de arrays y reto de las funciones/Ejercicio con Unshift/Scripts.js
+++
@@ -1,3 +0,0 @@
-let juegos = ["Resident Evil" , "God of war", "God of war 2", "GTA San Andreas"]; /*Arreglo originalmente de 4 elementos*/
-juegos.unshift("Dragon ball Xenoverse", "Detroit become human"); /*Mediante el método se agrega uno o más elementos al inicio del array, cambiando el array original, por lo tanto el tamaño del mismo*/
-console.log(juegos);
