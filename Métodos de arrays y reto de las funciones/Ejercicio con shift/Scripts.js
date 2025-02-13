810ce509bba0937dcb63f3c2262a52610fc2d1c0
samuelito1218 <165844479+samuelito1218@users.noreply.github.com>
2025-02-13 00:29:02 -0500

    Delete Métodos de arrays y reto de las funciones directory

--- Métodos de arrays y reto de las funciones/Ejercicio con shift/Scripts.js
+++
@@ -1,3 +0,0 @@
-let juegos = ["Resident Evil" , "God of war", "God of war 2", "GTA San Andreas"]; /*Arreglo originalmente de 4 elementos*/
-juegos.shift(); /*Mediante el método se  elimina el primer elemento del arreglo, y se muestra el resultado final por consola*/
-console.log(juegos);
