5dd90bfa190638262168f7c0c858b3b482d0706f
samuelito1218 <165844479+samuelito1218@users.noreply.github.com>
2025-02-13 00:27:40 -0500

    Delete Métodos de arrays y reto de las funciones/Ejercicio con Join directory

--- Métodos de arrays y reto de las funciones/Ejercicio con Join/Scripts.js
+++
@@ -1,6 +0,0 @@
-let juegos = ["Resident Evil", "God of War", "GTA San Andreas","The last of us"]; /*Un arreglo definido */
-
-let resultado = juegos.join(",") /*El método permite separar los elementos del arreglo teniendo en cuenta la "," como delimitador */
- 
-console.log(resultado);
-
