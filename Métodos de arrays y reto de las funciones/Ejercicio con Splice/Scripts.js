7b34ad1270b162e0849a7830f25f1a39ee45a0c8
samuelito1218 <165844479+samuelito1218@users.noreply.github.com>
2025-02-13 00:28:45 -0500

    Delete Métodos de arrays y reto de las funciones/Ejercicio con Splice directory

--- Métodos de arrays y reto de las funciones/Ejercicio con Splice/Scripts.js
+++
@@ -1,8 +0,0 @@
-let juegos = ["Resident Evil", "God of War", "GTA San Andreas"]; /*Un arreglo definido */
-
-juegos.splice(2, 1); /*Permite eliminar el juego en la posición 2, en este caso GTA */
-
-juegos.splice(1,0,"The last of us"); /*Permite agregar un juego a la posición 1, moviendo los otros eleementos */
-
-console.log(juegos);
-
